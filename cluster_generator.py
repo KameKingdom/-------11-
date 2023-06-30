dbb8132a5cd0c61b9d8cf35c23f2ca9649273c68
KameKingdom <117248688+KameKingdom@users.noreply.github.com>
2023-06-30 17:10:57 +0900

    1.0.3

--- cluster_generator.py
+++ cluster_generator.py
@@ -1,14 +1,12 @@
 import numpy as np
 import csv
+import random
+import matplotlib.pyplot as plt
 
 # クラスタの中心座標
 cluster_centers = [(2, 2), (8, 8), (5, 5)]
-
-# クラスタごとのデータ数
-data_per_cluster = 150
-
-# ノイズの標準偏差
-noise_std = 0.3
+data_per_cluster = 200  # 1クラスタあたりのデータ数
+noise_std = 0.5  # ノイズの標準偏差
 
 # データ生成
 data = []
@@ -18,6 +16,14 @@ for center in cluster_centers:
     cluster_data = np.column_stack((x, y))
     data.append(cluster_data)
 
+# ノイズ生成
+noise_num = 50
+for _ in range(noise_num):
+    x = random.uniform(0, 10)
+    y = random.uniform(0, 10)
+    noise_data = np.column_stack((x, y))
+    data.append(noise_data)
+
 data = np.vstack(data)
 
 # CSVファイルへの書き込み
@@ -25,3 +31,10 @@ with open('cluster_data.csv', 'w', newline='') as file:
     writer = csv.writer(file)
     writer.writerow(['x', 'y'])
     writer.writerows(data)
+
+# 二次元分布の表示
+plt.scatter(data[:, 0], data[:, 1])
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('Data Distribution')
+plt.show()
