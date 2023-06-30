4ee265cab1eb6f8db7801ae850886a80c52dee99
KameKingdom <117248688+KameKingdom@users.noreply.github.com>
2023-07-01 02:13:30 +0900

    Update N_means_postalization.py

--- N_means_postalization.py
+++ N_means_postalization.py
@@ -1,15 +1,26 @@
 import cv2
 import numpy as np
-from sklearn.cluster import KMeans
+import random
+
+def kmeans(X, n_clusters, random_state=0, max_iter=300):
+    random.seed(random_state)
+    centers = X[random.sample(range(X.shape[0]), n_clusters)]
+    for _ in range(max_iter):
+        labels = np.argmin(np.linalg.norm(X[:, np.newaxis] - centers, axis=-1), axis=-1)
+        new_centers = np.array([np.mean(X[labels == i], axis=0) for i in range(n_clusters)])
+        if np.all(centers == new_centers):
+            break
+        centers = new_centers
+    return labels, centers
 
 def posterize_image(image_path, n_colors):
     img = cv2.imread(image_path)
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
     pixels = img.reshape(-1, 3)
-    kmeans = KMeans(n_clusters=n_colors, random_state=0).fit(pixels)
+    labels, centers = kmeans(pixels, n_clusters=n_colors, random_state=0)
 
-    new_pixels = kmeans.cluster_centers_[kmeans.labels_].astype(int)
+    new_pixels = centers[labels].astype(int)
     new_img = new_pixels.reshape(img.shape)
 
     new_img = cv2.cvtColor(new_img, cv2.COLOR_RGB2BGR)
