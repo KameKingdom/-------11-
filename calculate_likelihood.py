37340bc50521a133864d781bfc24bce84643f67b
KameKingdom <117248688+KameKingdom@users.noreply.github.com>
2023-07-07 02:01:30 +0900

    Update

--- calculate_likelihood.py
+++ calculate_likelihood.py
@@ -1,15 +1,17 @@
 import find_path
+from itertools import product
+import csv
 
 def calculate_likelihood(routes, transitions, probabilities, observations):
     likelihood = 0.0
     for n, transition in zip(range(len(transitions)), transitions):
         transition_probability = 1.0
-        print(f"P {n+1}:", end=" ")
         for i in range(len(transition)):
             transition_probability *= (
                 probabilities[routes[transition[i]]][0]
                 * probabilities[routes[transition[i]]][1 if observations[i] == 1 else 2]
             )
+            """
             if i != len(transition) - 1:
                 print(
                     f"{probabilities[routes[transition[i]]][0]} × {probabilities[routes[transition[i]]][1 if observations[i] == 1 else 2]}",
@@ -20,9 +22,10 @@ def calculate_likelihood(routes, transitions, probabilities, observations):
                     f"{probabilities[routes[transition[i]]][0]} × {probabilities[routes[transition[i]]][1 if observations[i] == 1 else 2]}",
                     end="",
                 )
+            """
         likelihood += transition_probability
-        print(f" = {round(transition_probability, 5)}")
-    print(f"P({observations} | M) = {round(likelihood, 5)}")
+        #print(f" = {round(transition_probability, 5)}")
+    #print(f"P({observations} | M) = {round(likelihood, 5)}")
 
     return likelihood
 
@@ -81,11 +84,21 @@ def convert_observation(seq):
             converted_list.append(0)
     return converted_list
 
+def generate_combinations(characters, max_length):
+    combinations = []
+    for length in range(2, max_length + 1):
+        for item in product(characters, repeat=length):
+            combinations.append(''.join(item))
+    return combinations
+
+
+
 if __name__ == "__main__":
-    sequential_progression = input("input sequential progression >> ")
+    #sequential_progression = input("input sequential progression >> ")
     start_node = "1"
     end_node = "5"
-    steps = len(sequential_progression)
+    characters = "fg"
+
     graph = {
         "1": ["1", "2", "3"],
         "2": ["2", "3", "4"],
@@ -93,15 +106,34 @@ if __name__ == "__main__":
         "4": ["4", "5"],
         "5": ["5"],
     }
-    paths = find_path.find_paths(graph, start_node, end_node, steps)
-    transitions = find_path.translate_transitions(paths)
-    observations = convert_observation(sequential_progression)
+    combinations = generate_combinations(characters, int(input("num >> ")))
+    print(combinations)
 
-    beatlesProb = calculate_likelihood(routes, transitions, beatlesDictionary, observations)
-    rollingstonesProb = calculate_likelihood(routes, transitions, rollingstonesDictionary, observations)
-    if beatlesProb > rollingstonesProb:
-        print("ビートルズの楽曲が妥当")
-    elif rollingstonesProb > beatlesProb:
-        print("ローリングストーンズの楽曲が妥当")
-    else:
-        print("どちらとも言えない")
+    # Open CSV file for writing
+    with open('output.csv', 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+
+        # Write header
+        writer.writerow(['文字列', '結果', 'beatlesの確率', 'rolling stonesの確率'])
+
+        for combination in combinations:
+            sequential_progression = combination
+            steps = len(sequential_progression)
+            
+            paths = find_path.find_paths(graph, start_node, end_node, steps)
+            transitions = find_path.translate_transitions(paths)
+            observations = convert_observation(sequential_progression)
+
+            beatlesProb = calculate_likelihood(routes, transitions, beatlesDictionary, observations)
+            rollingstonesProb = calculate_likelihood(routes, transitions, rollingstonesDictionary, observations)
+            
+            # Decide the result
+            if beatlesProb > rollingstonesProb:
+                result = "beatles"
+            elif rollingstonesProb > beatlesProb:
+                result = "rolling stones"
+            else:
+                result = "both"
+
+            # Write to CSV file
+            writer.writerow([sequential_progression, result, beatlesProb, rollingstonesProb])
